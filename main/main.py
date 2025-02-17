14f50280d4828cfc3c14734120ff9814ce6caf3f
Émile Gendron <2351877@cegeplimoilou.ca>
2025-02-17 12:10:52 -0500

    ajout de la feuille de temps et du fichier principal avec quelque imports

---
+++ main/main.py
@@ -0,0 +1,13 @@
+import pathlib
+import getpass
+import time
+from bs4 import BeautifulSoup
+from proton.api import Session, ProtonError
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
